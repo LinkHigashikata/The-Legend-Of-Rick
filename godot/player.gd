f3b73e18f2d91a4a8d70a17ca6275a0bb9116a3e
Link <delomarthur@gmail.com>
2023-11-16 12:38:20 +0100

    début godot pour faire bouger le rick
    (ça marche zéro)

---
+++ godot/player.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+@export var speed = 400
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("go_right"):
+		velocity.x += 1
+	elif Input.is_action_just_pressed("go_left"):
+		velocity.x -= 1
+	elif Input.is_action_just_pressed("go_down"):
+		velocity.y -= 1
+	elif Input.is_action_just_pressed("go_up"):
+		velocity.y += 1
+		
+	if velocity.lenght() > 0:
+		velocity = velocity.normalized() * speed;
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
