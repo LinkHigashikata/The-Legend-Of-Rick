27e56de9e0e91cd71c9eddb0376e465d8aed22f3
symsym <sym.playing@gmail.com>
2023-11-16 17:19:22 +0100

    fix déplacement + modifs scènes
    Le player est passé de Area2D à CharacterBody2D (plus simple pour le mouvement etc)
    Ajout des flèches directionnelles comme déplacement
    Création d'une scène (principale ?)

--- godot/player.gd
+++ godot/player.gd
@@ -1,27 +1,19 @@
-extends Area2D
+extends CharacterBody2D
 
-@export var speed = 400
+@export var speed: int = 400
 var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
 
-func process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("go_right"):
-		velocity.x += 1
-	elif Input.is_action_just_pressed("go_left"):
-		velocity.x -= 1
-	elif Input.is_action_just_pressed("go_down"):
-		velocity.y -= 1
-	elif Input.is_action_just_pressed("go_up"):
-		velocity.y += 1
-		
+func handleInput():
+	var moveDirection = Input.get_vector("go_left", "go_right", "go_up", "go_down")
+	velocity = moveDirection * speed
+
+func _physics_process(delta):
+	handleInput()
+	move_and_slide()
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed;
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
-	
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
