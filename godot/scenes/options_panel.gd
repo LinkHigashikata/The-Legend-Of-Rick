ffbd9515f223dd3fc3ca2ea9a2c7956990b39fa2
Link <delomarthur@gmail.com>
2023-11-20 12:45:28 +0100

    Tentative de la fenêtre options (marche pas, problème avec le grab_focus())

---
+++ godot/scenes/options_panel.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer1/Start.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_global_settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_video_settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_sound_settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scenes/home.tscn")
