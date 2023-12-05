731926f1dd23e14078171637f6c8641b4703f729
symsym <sym.playing@gmail.com>
2023-12-05 23:25:23 +0100

    codage bancal du rick roll

--- godot/player.gd
+++ godot/player.gd
@@ -2,13 +2,16 @@ extends CharacterBody2D
 
 @export var speed: int = 400
 var screen_size
-
+var roll_vector: Vector2 = Vector2.RIGHT
 func _ready():
 	screen_size = get_viewport_rect().size
 
 func handleInput():
 	var moveDirection = Input.get_vector("go_left", "go_right", "go_up", "go_down")
 	velocity = moveDirection * speed
+	update_roll_vector()
+	if Input.is_action_just_pressed("ui_roll"):
+		set_roll_speed()
 
 func _physics_process(_delta):
 	handleInput()
@@ -25,3 +28,9 @@ func _physics_process(_delta):
 		$AnimatedSprite2D.animation = "walk_forward"
 	elif velocity.y < 0:
 		$AnimatedSprite2D.animation = "walk_back"
+
+func update_roll_vector():
+	roll_vector = velocity
+	
+func set_roll_speed():
+	velocity = roll_vector * 30 * 1.5
